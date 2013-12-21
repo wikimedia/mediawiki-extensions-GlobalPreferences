c9b634f1ac893b45fa49e4f95ddd67e103b56321
Kunal Mehta <legoktm@gmail.com>
2013-12-20 17:25:55 -0800

    SQLite support
    Bug: 58736
    Change-Id: I42621e2232dff96913498a0da8145091dadc0d92

--- schema.sql
+++ schema.sql
@@ -1,7 +1,5 @@
 CREATE TABLE global_preferences (
   gp_user INT(11) NOT NULL,
   gp_property VARBINARY(255) NOT NULL,
-  gp_value BLOB,
-  PRIMARY KEY ( gp_user, gp_property ),
-  KEY ( gp_property )
+  gp_value BLOB
 ) /*$wgDBTableOptions*/;
