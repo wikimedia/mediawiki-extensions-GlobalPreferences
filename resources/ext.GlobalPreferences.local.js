6d4b796da6a0ec6c4eb57a437216cada88ecb09e
Sam Wilson <sam@samwilson.id.au>
2018-01-11 12:09:14 +0800

    Enable/disable related preference field with local exception checkbox.
    When the local-exception checkbox is changed, enable or disable
    the related preference field. Also takes into account OOUI fields
    such as is used by BetaFeatures.
    
    Bug: T178267
    Depends-On: Ib04701464e4869c0b89387b7291d2eb6326e3fc0
    Change-Id: I3dcd540a29635614aaa9404b8482d1493fda0c3c

--- resources/ext.GlobalPreferences.local.js
+++ resources/ext.GlobalPreferences.local.js
@@ -1,1 +1,26 @@
-// @TODO re-enable global preferences. T178267
+( function ( mw, $, OO ) {
+	'use strict';
+
+	/**
+	 * Enable and disable the related preference field when selecting the local exception checkbox.
+	 */
+	$( ':input.mw-globalprefs-local-exception' ).on( 'change', function () {
+		var localExName, prefName, enabled, $prefInput, oouiWidget;
+		// Figure out what the preference name is by stripping the local exception suffix.
+		localExName = $( this ).attr( 'name' );
+		prefName = localExName.substr( 0, localExName.length - '-local-exception'.length );
+		enabled = $( this ).prop( 'checked' );
+		$prefInput = $( ':input[name="' + prefName + '"]' );
+
+		if ( $prefInput.parent( '.oo-ui-widget' ).length > 0 ) {
+			// First see if this is a OOUI field.
+			oouiWidget = OO.ui.infuse( $prefInput.parent( '.oo-ui-widget' ) );
+			oouiWidget.setDisabled( !enabled );
+
+		} else {
+			// Otherwise treat it as a normal form input.
+			$prefInput.prop( 'disabled', !enabled );
+		}
+	} );
+
+}( mediaWiki, jQuery, OO ) );
