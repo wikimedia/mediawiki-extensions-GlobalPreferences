a7800ed20db393c0b638b02ad496575a23272b9c
Reedy <reedy@wikimedia.org>
2021-11-03 14:55:46 +0000

    Make SQLite specific gp_user unsigned patch
    Bug: T294921
    Change-Id: I02719df6689d7e8b42e985904de17661e4ba52d9

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -188,7 +188,7 @@ class Hooks {
 				);
 				$updater->modifyExtensionField( 'global_preferences',
 					'gp_user',
-					"$sqlPath/patch-gp_user.sql"
+					"$sqlPath/$type/patch-gp_user-unsigned.sql"
 				);
 			}
 		}
