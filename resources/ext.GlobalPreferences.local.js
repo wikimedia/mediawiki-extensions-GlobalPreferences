11efdbfceaf79c7efe1608e4091de4b2b918e4c0
Ed Sanders <esanders@wikimedia.org>
2025-02-19 14:46:41 +0000

    ESLint: Update to ES6 and autofix
    Change-Id: Ibe7e4bd080dca63f99fd97eb8633dae5efd6f3f3

--- resources/ext.GlobalPreferences.local.js
+++ resources/ext.GlobalPreferences.local.js
@@ -6,10 +6,10 @@
 	 *
 	 * @param {string} index Search index
 	 */
-	mw.hook( 'prefs.search.buildIndex' ).add( function ( index ) {
-		Object.keys( index ).forEach( function ( key ) {
-			index[ key ].forEach( function ( item ) {
-				var $localException = item.$field.next( '.mw-globalprefs-local-exception' );
+	mw.hook( 'prefs.search.buildIndex' ).add( ( index ) => {
+		Object.keys( index ).forEach( ( key ) => {
+			index[ key ].forEach( ( item ) => {
+				const $localException = item.$field.next( '.mw-globalprefs-local-exception' );
 				if ( $localException.length ) {
 					item.$field = item.$field.add( $localException );
 				}
